c175e4d099e8d981c741796a00620dbc85d33780
SOHEE <118507151+devheee@users.noreply.github.com>
2023-02-15 22:44:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function () {
 
     $('.main_slide').slick({
         arrows: false,
+        autoplay: true,
     });
 
     $('#mainVisual .main_arrows i:first-child').on('click', function () {
@@ -10,4 +11,20 @@ $(function () {
     $('#mainVisual .main_arrows i:last-child').on('click', function () {
         $('.main_slide').slick('slickNext')
     });
+
+    $('.product_slide').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $('.map_menu>li').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.inner .map').eq(idx).addClass('on').siblings().removeClass('on')
+    })
+
 })
